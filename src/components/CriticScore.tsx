ce2f8bd07d5179655208a9fe8af212ea758938d3
kisakyegordon <gordon56692@gmail.com>
2025-11-27 16:27:03 -0600

    Adds optimized cropped images

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -6,7 +6,6 @@ interface Props {
 
 function CriticScore({ score }: Props) {
   let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
-  console.log(color)
   return (
     <Badge
       colorPalette={color}
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import type { Game } from "../hooks/useGames";
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/getCroppedImageUrl";
 
 interface Props {
   game: Game;
@@ -10,7 +11,7 @@ interface Props {
 function GameCard({ game }: Props) {
   return (
     <Card.Root borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} fit="cover" />
+      <Image src={getCroppedImageUrl(game.background_image)} fit="cover" />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justify="space-between">
